7b26823117e873e3eea5c3364490824160255f72
nch8501 <nch8501@rit.edu>
2018-02-27 12:14:49 -0500

    Added enemies who are destroyed when sword touches them.

---
+++ Unity Project/Assets/Scripts/baseEnemyScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class baseEnemyScript : MonoBehaviour {
+
+    public GameObject baseEnemyObject;
+
+    public GameObject playerObject;
+    public GameObject playerWeapon;
+
+    public bool colliding;
+    public int col = 0;
+
+
+	// Use this for initialization
+	void Start () {
+        //setup player weapon
+        playerWeapon = playerObject.transform.GetChild(0).transform.GetChild(0).gameObject;
+        colliding = true;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    //check if enemy is colliding with something
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Colliding");
+        //check if player sword is being used
+        if (collision.gameObject == playerWeapon)
+        {
+            if(playerWeapon.GetComponent<WeaponScript>().isAttacking)
+            {
+                
+                Destroy(baseEnemyObject);
+            }
+            
+        }
+
+
+        
+    }
+
+}
---
+++ Unity Project/Assets/Scripts/enemyColliding.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyColliding : MonoBehaviour {
+    public int col = 10;
+
+    public void Start()
+    {
+        col = 100;
+    }
+
+
+
+    //check if enemy is colliding with something
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //check if player sword is being used
+
+
+
+        Debug.Log("Colliding");
+    }
+
+
+}
