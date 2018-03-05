12622a036a9f53eb424d20edd67f83bcedaa6b02
nch8501 <nch8501@rit.edu>
2018-03-05 14:25:24 -0500

    Camera now moves when player moves far enough from the camera

---
+++ Unity Project/Assets/Scripts/cameraController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Controls the Camera
+public class cameraController : MonoBehaviour {
+
+    GameObject playerObject;    //player
+
+    // Use this for initialization
+    void Start () {
+        //setup playerObject
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        CameraDistance();
+	}
+
+    //checks if the player is too far from the camera and moves it
+    void CameraDistance()
+    {
+        Vector3 playerPos = playerObject.transform.position;
+
+        float xBuffer = 5.0f;
+        float yBuffer = 3.0f;
+
+        if(playerPos.x - transform.position.x >= xBuffer)
+        {
+            //move camera
+            transform.position += new Vector3(0.09f, 0.0f, 0.0f);
+        }
+        if(transform.position.x - playerPos.x >= xBuffer)
+        {
+            //move camera
+            transform.position -= new Vector3(0.09f, 0.0f, 0.0f);
+        }
+
+        if (playerPos.y - transform.position.y >= yBuffer)
+        {
+            //move camera
+            transform.position += new Vector3(0.0f, 0.09f, 0.0f);
+        }
+        if (transform.position.y - playerPos.y >= yBuffer)
+        {
+            //move camera
+            transform.position -= new Vector3(0.0f, 0.09f, 0.0f);
+        }
+    }
+}
