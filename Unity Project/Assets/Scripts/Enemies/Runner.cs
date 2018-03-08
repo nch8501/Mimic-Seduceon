7d1671254aac33a5e11ea4d3ba4b7b596eef11ef
BEN\Ben <btf6119@g.rit.edu>
2018-03-08 00:39:32 -0500

    OOOOOOOOOO SPOOPY

---
+++ Unity Project/Assets/Scripts/Enemies/Runner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runner : Enemy {
+    // Purpose: Runner Enemy
+    // Limitations: None
+
+    // Fields
+
+    // Wow
+    // There is nothing here
+    // What a special fucking snowflake
+}
--- Unity Project/Assets/Scripts/Managers/EnemyManager.cs
+++ Unity Project/Assets/Scripts/Managers/EnemyManager.cs
@@ -11,27 +11,25 @@ public class EnemyManager : MonoBehaviour {
     private GameObject[] enemyPrefabs;
 
     // List of places for enemies to spawn
-    private List<GameObject> spawnPoints;
+    private Transform[] spawnPoints;
 
     public List<GameObject> enemies = new List<GameObject>();   //list of all active enemies
 
 
 	// Use this for initialization
 	void Start () {
-        // Create List of spawn points
-        spawnPoints = new List<GameObject>();
-
         // Get the spawn Points
-        foreach (Transform child in transform)
-        {
-            spawnPoints.Add(child.gameObject);
-        }
+        spawnPoints = gameObject.GetComponentsInChildren<Transform>();
 
         // Spawn a random enemy at each of the spawn points
         Quaternion zero = Quaternion.Euler(0, 0, 0);
-        foreach (GameObject spawner in spawnPoints)
+        for (int i = 1; i < spawnPoints.Length; i++)
         {
-            enemies.Add(Instantiate(RandomEnemyPrefab(), spawner.transform.position, zero));
+            GameObject enemy = RandomEnemyPrefab();
+            if (enemy != null)
+            {
+                enemies.Add(Instantiate(enemy, spawnPoints[i].position, zero));
+            }
         }
     }
 	
@@ -79,9 +77,16 @@ public class EnemyManager : MonoBehaviour {
     /// <summary>
     /// Get a random enemy from the prefab array
     /// </summary>
-    /// <returns>An Enemy from the Prefab array</returns>
+    /// <returns>An Enemy from the Prefab array (Null if no enemy)</returns>
     private GameObject RandomEnemyPrefab()
     {
-        return enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+        // Negative 1 represents no enemy
+        // NO ENEMY DISABLED
+        int enemy = Random.Range(0, enemyPrefabs.Length);
+        if (enemy != -1)
+        {
+            return enemyPrefabs[enemy];
+        }
+        return null;
     }
 }
